912cf8202a316d7500f543656acec41d40d9dc8b
Majid <Me>
2023-07-30 11:53:02 -0700

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,35 @@
+import useGenres, { Genre } from "../hooks/useGenres";
+
+const GenreList = () => {
+  const persianGenre: { [key: string]: string } = {
+    action: "اکشن",
+    indie: "ایندی",
+    adventure: "ماجراجویی",
+    "role-playing-games-rpg": "جهان باز",
+    strategy: "استراتژیک",
+    shooter: "شوتر",
+    casual: "معمولی",
+    simulation: "شبیه سازی",
+    puzzle: "پازل",
+    arcade: "آرکید",
+    platformer: "پلتفورمی",
+    "massively-multiplayer": "مولتی پلیر",
+    racing: "ماشین مسابقه",
+    sports: "ورزشی",
+    family: "خانوادگی",
+    fighting: "جنگی",
+    "board-games": "تخته ای",
+    card: "کارتی",
+    educational: "آموزشی",
+  };
+  const { genres, error, isLoading } = useGenres();
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{persianGenre[genre.slug]}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreList;
---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+export interface Genre {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+interface fetchGenre {
+  count: number;
+  results: Genre[];
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    apiClient
+      .get<fetchGenre>("/genres", { signal: controller.signal })
+      .then((response) => {
+        setLoading(false);
+        setGenres(response.data.results);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;

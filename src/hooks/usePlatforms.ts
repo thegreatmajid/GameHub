d6dcbc6d77bc54d6b8aa44139757e97a653778d3
Majid <Me>
2023-08-08 02:24:01 -0700

    Refactoring: Comment times

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -14,7 +14,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: 24 * 60 * 60 * 1000, //24h
     initialData: { count: platforms.length, next: null, results: platforms },
   });
 

20460bcf743d6a5de4d26eada17e294803c31cb8
Majid <Me>
2023-08-12 11:39:17 -0700

    Refactoring-move interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,13 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import APIClient from "../services/api-client";
-
-export interface Genre {
-  id: number;
-  name: string;
-  slug: string;
-  image_background: string;
-}
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
 
